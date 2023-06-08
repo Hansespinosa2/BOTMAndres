3916b9ac1b211ccd141c938f3ac01f143587eb0c
Hansespinosa2 <133429105+Hansespinosa2@users.noreply.github.com>
2023-06-08 17:09:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
 
+
 from vehicle_pickups import create_pickups,create_vehicles
 import optimize
 import create_map
 from routes import process_worksheet
-from google_sheets import connect_to_excel
-import pandas as pd
-from geocoding import find_lat,find_long
-from gspread import Cell
-worksheet = connect_to_excel("C\\Users\\hanse\\Downloads\\BOTM Database")
-rows = worksheet.get_all_values()
 
-# Convert to a DataFrame
-columns = rows[0]
-df = pd.DataFrame.from_records(rows[1:], columns=columns) 
-new_df = df[(df['LAT'] == '') | (df['LONG'] == '')]
 
-# Calc missing coordinates
-cell_list = []
-lat_col = df.columns.get_loc('LAT') + 1
-long_col = df.columns.get_loc('LONG') + 1
-for index, row in new_df.iterrows():
-  #df['LAT'] = df.apply(lambda row: find_lat(row['ADDRESS']) if row['LAT'] == '' else row['LAT'], axis=1)
-  cell_list.append(Cell(row=index+2, col=lat_col, value=find_lat(row['ADDRESS'])))
-  cell_list.append(Cell(row=index+2, col=long_col, value=find_long(row['ADDRESS'])))
-
-if cell_list != []: worksheet.update_cells(cell_list)
-# Global variables used in the code
+ # Global variables used in the code
 MAX_VEHICLES = 3
 MAX_CAPACITY = 5000
 DEFAULT_SERVICE_TIME = 20*60 # Assume 20 minutes at each site
@@ -43,9 +24,6 @@ Vehicle2T = 26
 Vehicle3T = 143
 avgDuration = (Vehicle1T + Vehicle2T + Vehicle3T)/3
 #FIGURE OUT __INIT__
-
-
-
 
 # Assuming you have the necessary variables defined
 df_week = process_worksheet()
