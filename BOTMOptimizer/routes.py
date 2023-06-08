b90d93e12a639373eedff4294cc32617aa075316
Hansespinosa2 <133429105+Hansespinosa2@users.noreply.github.com>
2023-06-08 14:44:25 -0400

    Update routes.py

--- BOTMOptimizer/routes.py
+++ BOTMOptimizer/routes.py
@@ -1,19 +1,14 @@
-from os import truncate
-from folium.plugins import BeautifyIcon
 import pandas as pd
-from gspread import authorize
-#from gspread.models import Cell
-from oauth2client.service_account import ServiceAccountCredentials
-#from google.colab import drive
-from pandas._libs.hashtable import value_count
-from google_sheets import connect_to_sheets
 import re
+from folium.plugins import BeautifyIcon
+from gspread import authorize
+from oauth2client.service_account import ServiceAccountCredentials
+from google_sheets import connect_to_sheets, connect_to_excel
+from os import truncate
 
 
 def process_worksheet():
-    # Access Google Sheet
-    gclient = connect_to_sheets()
-    worksheet = gclient.open("BOTM Database").sheet1
+    worksheet = connect_to_excel("C\\Users\\hanse\\Downloads\\BOTM Database")
     rows = worksheet.get_all_values()
     columns = rows[0]
     df = pd.DataFrame.from_records(rows[1:], columns=columns)
@@ -29,8 +24,6 @@ def process_worksheet():
     df_thurs = df[df['THURS'] != ''].sort_values(by=['THURS'])
     df_fri = df[df['FRI'] != ''].sort_values(by=['FRI'])
     df_week = [df_mon, df_tues, df_wed, df_thurs, df_fri]
-
-   
 
     # Additional processing logic or functions can be included here
 
