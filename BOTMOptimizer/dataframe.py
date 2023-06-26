34c9c83392eb31cc4a98ab579bd9a416d03ebe0b
Hansespinosa2 <133429105+Hansespinosa2@users.noreply.github.com>
2023-06-26 10:44:18 -0400

    Update dataframe.py

--- BOTMOptimizer/dataframe.py
+++ BOTMOptimizer/dataframe.py
@@ -1,16 +1,14 @@
 import pandas as pd
-# Access Google Sheet
 from google_sheets import connect_to_excel
 
-
+# Access Google Sheet
 BenWorksheet = connect_to_excel("C\\Users\\hanse\\Downloads\\BOTM Database")
 rows = BenWorksheet.get_all_values()
 
 # Convert to a DataFrame
 columns = rows[0]
-Bendf = pd.DataFrame.from_records(rows[1:], columns=columns) 
-##new_df = df[(df['LAT'] == '') | (df['LONG'] == '')]
+Bendf = pd.DataFrame.from_records(rows[1:], columns=columns)
 
-df_thurs = Bendf[Bendf['THURS']!=''].sort_values(by=['THURS'])
+df_thurs = Bendf[Bendf['THURS'] != ''].sort_values(by=['THURS'])
 
 Bendf
