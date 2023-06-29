336605440d1e5eec1bac48a1f9c9e0f58bf88524
Hansespinosa2 <133429105+Hansespinosa2@users.noreply.github.com>
2023-06-28 23:20:53 -0400

    Update dataframe.py

--- BOTMOptimizer/dataframe.py
+++ BOTMOptimizer/dataframe.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from google_sheets import connect_to_excel
+from path_directories import DATABASE_EXCEL
 
 # Access Google Sheet
-BenWorksheet = connect_to_excel("C\\Users\\hanse\\Downloads\\BOTM Database")
+BenWorksheet = connect_to_excel(DATABASE_EXCEL)
 rows = BenWorksheet.get_all_values()
 
 # Convert to a DataFrame
